523aaafa17cd1b2dd56b3148bb70c1bd823389fb
Caroline Ribeiro <dev.caroline.ribeiro@gmail.com>
2021-11-25 20:33:43 -0300

    wip

--- src/controller/categoria.controller.js
+++ src/controller/categoria.controller.js
@@ -1,16 +1,10 @@
 const { addCategoria, getAllCategoria, getById , deleteById : deleteByIdCategoria } = require('../service/categoria.service'); 
 
 const addItemCategoria = async (req , res) => { 
-    if (req.url.endsWith('/')) {
-        res.redirect(301, req.url.slice(0, 44))
-    }
     return res.status(200).json(await addCategoria(req.body));
 }
 
 const selectAllCategoria = async (req , res) => { 
-    if (req.url.endsWith('/')) {
-        res.redirect(301, req.url.slice(0, 44))
-    }
     return res.status(200).json(await getAllCategoria(req.body));
 }
 
